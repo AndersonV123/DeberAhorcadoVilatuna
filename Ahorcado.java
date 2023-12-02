10ae8ec8015a9f5c133503b6386b9036fa3e088d
AndersonV123 <150813921+AndersonV123@users.noreply.github.com>
2023-12-01 21:27:33 -0500

    Palabra Intentos
    Palabra seleccionada para jugar y numero de intentos

--- Ahorcado.java
+++ Ahorcado.java
@@ -8,3 +8,17 @@ public class Ahorcado {
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
         boolean jugar = true;
+
+        while (jugar) {
+            String palabraSeleccionada = palabras[rand.nextInt(palabras.length)];
+            char[] palabraSecreta = new char[palabraSeleccionada.length()];
+            for (int i = 0; i < palabraSecreta.length; i++) {
+                palabraSecreta[i] = '_';
+            }
+
+            int intentos = 7;
+            boolean palabraDescubierta = false;
+            boolean[] letrasUsadas = new boolean[26];
+        }
+    }
+}
